0f654a4a6a5346b2ee93958ae69e36e1dae2103a
JoKing <joebryant614@gmail.com>
2021-07-07 14:19:02 -0700

    reply button works

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,40 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+//set up Reply schema to hold this data:
+// The name of the reply writer
+// the reply body
+// A timestamp of when the commment was created
 
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 //set up comment schema to hold this data:
-    // The name of the comment writer
-    // the comment body
-    // A timestamp of when the commment was created
-    
+// The name of the comment writer
+// the comment body
+// A timestamp of when the commment was created
+
 const CommentSchema = new Schema({
     writtenBy: {
         type: String
@@ -14,10 +44,25 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
+);
+
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
 });
 
-const Comment = model('Comment',CommentSchema);
+
+const Comment = model('Comment', CommentSchema);
 
 module.exports = Comment;
